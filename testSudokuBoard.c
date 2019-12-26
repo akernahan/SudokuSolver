d70cbec7f1ae76c2d9fe00b39171ab052e2b10aa
Andrew Kernahan <z5164699@student.unsw.edu.au>
2019-12-26 12:26:26 +1100

    Got the string working

--- testSudokuBoard.c
+++ testSudokuBoard.c
@@ -1,23 +1,87 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
+#include <string.h>
 
 #include "sudoku.h"
 
+#define MAX_STRING	128
+
+#define BIGINNER	5
+#define EASY		0
+#define MEDIUM		0
+#define HARD		0
+#define EXPERT		0
+#define MASTER		0
+
+struct node {
+	char*			filepath;
+	struct node*	next;
+};
+
+struct node* create_node(char* str) {
+	struct node *new = malloc(sizeof(struct node));
+	assert(new != NULL);
+
+	new->filepath = str;
+	new->next = NULL;
+	return new;
+}
+
+void destroy_list(struct node* l) {
+	if (l == NULL) {
+		return;
+	}
+
+	while (l != NULL) {
+		struct node* tmp = l;
+		l = l->next;
+		free(tmp);
+	}
+}
+
+void test_beginner() {
+	char str[MAX_STRING];
+
+	for (int i = 0; i < BIGINNER; i++) {
+		snprintf(str, MAX_STRING, "puzzles/Beginner/%d.txt", i);
+		printf("%s\n", str);
+	}
+	
+}
+
+void test_easy() {
+
+}
+
+void test_medium() {
+
+}
+
+void test_hard() {
+
+ }
+
+void test_expert() {
+
+}
+
+void test_master() {
+
+}
+
 int main(void) {
-	Board b = create_board("puzzles/0.txt");
+	// Board b = create_board("puzzles/Beginner/0.txt");
 	
-// 	printf("Board size: %lu\n", sizeof(struct sudoku_board));
-// // 	printf("Box size: 	%lu\n", sizeof(struct sudoku_box));
-// 	printf("Cell size:	%lu\n", sizeof(struct sudoku_cell));
-// 	printf("Bool size:	%lu\n", sizeof(b->board[0][0].possible));
-// 	printf("%p\n%p\n%d\n", b->board, b->board[0], 0);
-	
+	test_beginner();
+
 	//print_box(b, TOP_MIDDLE);
-	print_box(b, BOTTOM_RIGHT);
-	printf("\n");
+	// print_box(b, BOTTOM_RIGHT);
+	// printf("\n");
 	//b->board[8][8].value = 3;
-	print_box(b, BOTTOM_RIGHT);
+	// print_box(b, BOTTOM_RIGHT);
 	
-	//destroy_board(b);	
+	// destroy_board(b);	
 	return 0;
 }
